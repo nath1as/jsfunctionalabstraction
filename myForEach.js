4b8d46c2f64124523967f2db352a983aa96098d5
nathias <n@th1.as>
2018-08-12 15:45:31 +0200

    for each

---
+++ myForEach.js
@@ -0,0 +1,15 @@
+function myForEach(array, func) {
+  var i;
+  for (i = 0; i < array.length; i++) {
+    func(array[i]);
+  }
+}
+
+var min = Infinity;
+var getMin = function (value) {
+  min = value <= min ? value : min;
+};
+
+myForEach([4, 5, 12, 23, 3], getMin);    // undefined
+console.log(min);                        // 3
+
