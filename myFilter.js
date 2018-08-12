065dd0d5e17d0fbc314c9d829b786996fdf1dcdb
nathias <n@th1.as>
2018-08-12 16:44:26 +0200

    filter

---
+++ myFilter.js
@@ -0,0 +1,22 @@
+function myFilter(array, func) {
+  var result = [];
+
+  array.forEach(function (value) {
+    if (func(value)) {
+      result.push(value);
+    }
+  });
+
+  return result;
+}
+
+var isPythagoreanTriple = function (triple) {
+  return Math.pow(triple.a, 2) + Math.pow(triple.b, 2) === Math.pow(triple.c, 2);
+};
+
+myFilter([{ a: 3, b: 4,  c: 5 },
+          { a: 5, b: 12, c: 13 },
+          { a: 1, b: 2,  c: 3 },], isPythagoreanTriple);
+
+// returns [ { a: 3, b: 4, c: 5 }, { a: 5, b: 12, c: 13 } ]
+
