9e216a2cc05541089f5d844362cea538dde94c60
nathias <n@th1.as>
2018-08-14 13:32:42 +0200

    anagrams

---
+++ anagrams.js
@@ -0,0 +1,25 @@
+function anagram(word, list) {
+  return list.filter(function (candidate) {
+    return areAnagrams(candidate, word);
+  });
+}
+
+function areAnagrams(source, target) {
+  var sourceLetters = source.split('').sort();
+  var targetLetters = target.split('').sort();
+  return compareArrays(sourceLetters, targetLetters);
+}
+
+function compareArrays(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+
+  return array1.every(function (letter, index) {
+    return letter === array2[index];
+  });
+}
+
+console.log(anagram('listen', ['enlists', 'google', 'inlets', 'banana']));  // [ "inlets" ]
+console.log(anagram('listen', ['enlist', 'google', 'inlets', 'banana']));   // [ "enlist", "inlets" ]
+
