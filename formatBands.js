d0e2eb9b0c650d0b398f8d3aeffb11d3f1c4f181
nathias <n@th1.as>
2018-08-14 13:54:07 +0200

    format band data

---
+++ formatBands.js
@@ -0,0 +1,45 @@
+function processBands(bands) {
+  return bands.map(function (band) {
+    var capitalizedName = capitalizePhrase(band.name);
+    var newBandName = removeDotsInString(capitalizedName);
+
+    return {
+      name: newBandName,
+      country: 'Canada',
+      active: band.active,
+    };
+  });
+}
+
+function capitalizePhrase(phrase) {
+  var capitalizedPhrase = phrase.split(' ').map(function (word) {
+    return capitalizeString(word);
+  }).join(' ');
+
+  return capitalizedPhrase;
+}
+
+function capitalizeString(string) {
+  var initial = string[0].toUpperCase();
+  var rest = string.slice(1, string.length);
+  return initial + rest;
+}
+
+function removeDotsInString(string) {
+  return string.replace(/\./g, '');
+}
+
+var bands = [
+  { name: 'sunset rubdown', country: 'UK', active: false },
+  { name: 'women', country: 'Germany', active: false },
+  { name: 'a silver mt. zion', country: 'Spain', active: true },
+];
+
+console.log(processBands(bands));
+
+// should return:
+[
+  { name: 'Sunset Rubdown', country: 'Canada', active: false },
+  { name: 'Women', country: 'Canada', active: false },
+  { name: 'A Silver Mt Zion', country: 'Canada', active: true },
+];
