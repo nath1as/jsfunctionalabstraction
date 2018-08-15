c77af20c5e00b010a799df6dd8b345072812f00b
nathias <n@th1.as>
2018-08-15 13:16:18 +0200

    sum of sums

---
+++ sumOfSums.js
@@ -0,0 +1,14 @@
+function sumOfSums(array) {
+  return array.map(function (num, idx) {
+    return array.slice(0, idx + 1).reduce(function (sum, num) {
+      return sum + num;
+    });
+  }).reduce(function (sum, num) {
+    return sum + num;
+  });
+}
+
+console.log(sumOfSums([3, 5, 2]));        // (3) + (3 + 5) + (3 + 5 + 2) --> 21
+console.log(sumOfSums([1, 5, 7, 3]));     // (1) + (1 + 5) + (1 + 5 + 7) + (1 + 5 + 7 + 3) --> 36
+console.log(sumOfSums([4]));              // 4
+console.log(sumOfSums([1, 2, 3, 4, 5]));  // 35
